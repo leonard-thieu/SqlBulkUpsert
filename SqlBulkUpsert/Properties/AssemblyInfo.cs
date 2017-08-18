2b0ae53647859422f52dca71f82e7118c8fba86b
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-18 16:20:22 -0400

    Clean up AssemblyInfo.cs.

--- SqlBulkUpsert/Properties/AssemblyInfo.cs
+++ SqlBulkUpsert/Properties/AssemblyInfo.cs
@@ -2,11 +2,13 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SqlBulkUpsert")]
-[assembly: AssemblyProduct("SqlBulkUpsert")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
-[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+
+[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyProduct("SqlBulkUpsert")]
+
+[assembly: AssemblyTitle("SqlBulkUpsert")]
+
+[assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("SqlBulkUpsert.Tests")]
